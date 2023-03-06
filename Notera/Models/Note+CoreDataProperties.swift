31a4c7d2a07b6c04d1b0851c8e0cca204530fcaa
Emil Hörnlund <emilhornlund@Emils-MacBook-Pro.local>
2023-03-06 18:43:40 +0100

    Add CoreData models

---
+++ Notera/Models/Note+CoreDataProperties.swift
@@ -0,0 +1,63 @@
+//
+//  Note+CoreDataProperties.swift
+//  Notera
+//
+//  Copyright (c) 2023 Emil Hörnlund
+//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import Foundation
+import CoreData
+
+
+extension Note {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
+        return NSFetchRequest<Note>(entityName: "Note")
+    }
+
+    @NSManaged public var text: String
+    @NSManaged public var created: Date
+    @NSManaged public var modified: Date
+    @NSManaged public var tags: NSSet
+
+}
+
+// MARK: Generated accessors for tags
+extension Note {
+
+    @objc(addTagsObject:)
+    @NSManaged public func addToTags(_ value: Note)
+
+    @objc(removeTagsObject:)
+    @NSManaged public func removeFromTags(_ value: Note)
+
+    @objc(addTags:)
+    @NSManaged public func addToTags(_ values: NSSet)
+
+    @objc(removeTags:)
+    @NSManaged public func removeFromTags(_ values: NSSet)
+
+}
+
+extension Note : Identifiable {
+
+}
